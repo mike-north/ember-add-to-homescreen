a28e5b6a476ce50679d2f80e4c28a8e4b354befb
tsubomi imamura <timamura@linkedin.com>
2017-05-30 18:33:40 -0700

    Prepare for ember-cli-fastboot 1.0.0 release

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 /* jshint node: true */
 'use strict';
+const path = require('path');
+const fs = require('fs');
+
+const Funnel = require('broccoli-funnel');
+const map = require('broccoli-stew').map;
+const mergeTrees = require('broccoli-merge-trees');
+
 
 module.exports = {
   name: 'ember-add-to-homescreen',
   included: function(app) {
     this._super.included(app);
-
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      app.import(app.bowerDirectory + '/add-to-homescreen/src/addtohomescreen.js');
-    }
+    app.import('vendor/addtohomescreen.js');
     app.import(app.bowerDirectory + '/add-to-homescreen/style/addtohomescreen.css');
 
+  },
+  treeForVendor: function(defaultTree) {
+    const app = this._findHost();
+    const assetPath =path.join(__dirname, app.bowerDirectory, 'add-to-homescreen/src');
+    if (fs.existsSync(assetPath)) {
+      let browserHomeScreenJS = new Funnel(assetPath, {
+        files: ['addtohomescreen.js']});
+      browserHomeScreenJS = map(browserHomeScreenJS,
+        (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+      return new mergeTrees([defaultTree, browserHomeScreenJS]);
+    } else {
+      return defaultTree;
+    }
   }
 };
