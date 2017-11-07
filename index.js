987c0bcf8896d739b8d02f963552d2360b2cd42f
Michael North <michael.l.north@gmail.com>
2017-11-07 12:25:24 -1000

    remove debug

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 
 var Funnel = require('broccoli-funnel');
 var map = require('broccoli-stew').map;
-var debug = require('broccoli-stew').debug;
 var mergeTrees = require('broccoli-merge-trees');
 
 var A2H_ASSET_PATH = path.join(__dirname, 'node_modules', 'add-to-homescreen');
@@ -32,7 +31,7 @@ module.exports = {
     if (fs.existsSync(A2H_ASSET_PATH)) {
       var browserHomeScreenJS = notInFastboot(new Funnel(A2H_JS_PATH, { files: A2H_JS_FILES }));
       var browserHomeScreenCSS = new Funnel(A2H_CSS_PATH, { files: A2H_CSS_FILES });
-      return debug(new mergeTrees([defaultTree, browserHomeScreenJS, browserHomeScreenCSS]), { name: 'foo' });
+      return new mergeTrees([defaultTree, browserHomeScreenJS, browserHomeScreenCSS]);
     } else {
       throw new Error('add-to-homescreen was not found at ' + A2H_ASSET_PATH);
     }
