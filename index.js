dc866b3e23c6285a6a95e78fecb4829763828120
Mike North <michael.l.north@gmail.com>
2017-12-31 23:54:01 -0800

    TypeScript type definitions (#91)
    * TypeScript type definitions
    
    * Update yarn.lock
    
    * ignore .node_modules.ember-try/
    
    * Update dependencies
    
    * Remove broccoli debug
    
    * Drop 'ember-default' testing scenario

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 
 var Funnel = require('broccoli-funnel');
 var map = require('broccoli-stew').map;
-var debug = require('broccoli-stew').debug;
 var mergeTrees = require('broccoli-merge-trees');
 
 var A2H_ASSET_PATH = path.join(require.resolve('add-to-homescreen'), '..', '..');
@@ -34,7 +33,7 @@ module.exports = {
       var browserHomeScreenJS = notInFastboot(new Funnel(A2H_JS_PATH, { files: A2H_JS_FILES }));
       var browserHomeScreenCSS = new Funnel(A2H_CSS_PATH, { files: A2H_CSS_FILES });
       trees = trees.concat([browserHomeScreenJS, browserHomeScreenCSS]);
-      return debug(new mergeTrees(trees), { name: 'foo' });
+      return new mergeTrees(trees);
     } else {
       throw new Error('add-to-homescreen was not found at ' + A2H_ASSET_PATH);
     }
